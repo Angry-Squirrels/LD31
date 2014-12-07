13a7e1f719b6de98b32ff07b5c7a7d5c6d42baf7
Ynk33 <ytirand@gmail.com>
2014-12-07 20:55:17 +0100

    add sfx

--- unity-proj/Assets/scripts/MusicHandler.cs
+++ unity-proj/Assets/scripts/MusicHandler.cs
@@ -14,20 +14,22 @@ public class MusicHandler : MonoBehaviour {
 	private void newCycleHandler(int _cycle)
 	{
 		soundManager.StopAllMusics ();
-		print (_cycle);
 		switch (_cycle)
 		{
 			case 0:
 				soundManager.PlayMusic("dawn_theme");
+				soundManager.PlayMusic("cocorico");
 				break;
 			case 1:
 				soundManager.PlayMusic("day_theme");
+				soundManager.PlayMusic("birds");
 				break;
 			case 2:
 				soundManager.PlayMusic("twilight_theme");
 			break;
 			case 3:
 				soundManager.PlayMusic("night_theme");
+				soundManager.PlayMusic("wolf_howl");
 				break;
 			case 4:
 				soundManager.PlayMusic("main_theme");
