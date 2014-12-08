066bb53b3c80ba9b4b8b69a3b8967af2daa6a8eb
Thomas BAUDON <baudon.thomas@gmail.com>
2014-12-08 22:51:15 +0100

    gui debut

---
+++ unity-proj/Assets/scripts/GuiScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiScript : MonoBehaviour {
+
+	public MegaCarrot carrot;
+	public RectTransform bar;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float t = (float)carrot.mLife / (float)carrot.maxLife;
+		bar.transform.localScale = new Vector3(t,1.0f,1.0f);
+	}
+}
