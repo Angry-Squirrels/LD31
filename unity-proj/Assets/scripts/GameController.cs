400ea348f0a0c32ef52777f55e6d360a90baf708
Ynk33 <ytirand@gmail.com>
2014-12-09 03:17:40 +0100

    fixes

--- unity-proj/Assets/scripts/GameController.cs
+++ unity-proj/Assets/scripts/GameController.cs
@@ -43,7 +43,7 @@ public class GameController : MonoBehaviour {
 
 			Time.timeScale = 0;
 			gameUi.SetActive(false);
-			gameOverUI.updateText(mCurrentDay, nbCarrot, nbRabbitKilled);
+			gameOverUI.updateText(mDayManager.mCurrentDay, nbCarrot, nbRabbitKilled);
 			gameOverUI.gameObject.SetActive(true);
 		}
 	}
